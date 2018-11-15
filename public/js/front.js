341ce48ee94c7d5e4c93b63e927bd6306c9d48a4
Jack Morrissey <j.morrissey.16@gmail.com>
2018-11-15 01:21:32 -0600

    updated

--- public/js/front.js
+++ public/js/front.js
@@ -8,8 +8,9 @@ $("#scrape-button").on("click", function() {
     // Grab articles from database & append 
     $.getJSON("/articles", function(data) {
         data.forEach((article) => {
-            $("#resultsGuy").append("<a href='" + article.link + "'><h2>" +
-            article.headline + "</h2></a>\n<p>" + article.summary + "</p>")
+            console.log(article.link)
+            $("#resultsGuy").append("<a href='" + article.link + "'><h3>" +
+            article.headline + "</h3></a>\n<p>" + article.summary + "</p>")
         });
     });
 });
--- routes/routes.js
+++ routes/routes.js
@@ -21,48 +21,48 @@ router.get("/scrape", function (req, res) {
         // Load eth news html into cheerio
         let $ = cheerio.load(response.data);
         // Grab all article thumbnails 
-        $(".article-thumbnail__info").each(function(i, element) {
+        $(".article-thumbnail__info").each(function (i, element) {
             // Creat result object & use scrape results to declare properties
             let result = {};
 
             result.headline = $(this)
-            .children("h2").text();
+                .children("h2").text();
             result.summary = $(this)
-            .find(".article-thumbnail__info__summary").text();
+                .find(".article-thumbnail__info__summary").text();
             result.link = $(this)
-            .children("h2").attr("href"); 
-            
+                .find(".article-thumbnail__info__summary").children().attr("href");
+
             // Create new Article (for each) to store using result object from above
             db.Article.create(result)
-            .then(function(createdArticles) {
-                console.log(createdArticles);
-            })
-            .catch(function(err) {
-                console.log(err);
-            });
+                .then(function (createdArticles) {
+                    console.log(createdArticles);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
         });
         res.send();
     })
-    .catch((err) => {
-        res.json(err);
-        console.log(err);
-    });
+        .catch((err) => {
+            res.json(err);
+            console.log(err);
+        });
 });
 // Will get all stocks (news snippets/articles) from the database
-router.get("/articles", (req,res) => {
+router.get("/articles", (req, res) => {
     db.Article.find({})
-    .then(function(data) {
-        res.json(data);
-    })
-    .catch((err) => {
-        res.json(err);
-    });
+        .then(function (data) {
+            res.json(data);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
 });
 // Route for grabbing specific stock by id to & doing stuff with associated comments
-router.route("/articles/:id")    
-        .get( (req,res) => {})
-        .post( (req,res) => {})
-        // delete?
+router.route("/articles/:id")
+    .get((req, res) => { })
+    .post((req, res) => { })
+// delete?
 
 
 // Export router
